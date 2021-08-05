3a87f7e68a9f5ec5479152cb93a140afae6fefd4
camelot1399 <camelot1399@mail.ru>
2021-08-06 00:29:40 +0300

    kma2; add monitoring status server node.js

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <TestServer />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import TestServer from '../components/TestServerComponent.vue';
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    TestServer
   },
+  
 };
 </script>
